38ddc66e22e618661c1da956922c0a895a2b163a
Kogehame <kogehame@gmail.com>
2015-11-13 10:22:26 +0100

    Initial Commit

---
+++ Számológép/Szamologep.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Számológép
+{
+    partial class Szamologep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gomb1 = new System.Windows.Forms.Button();
+            this.gomb2 = new System.Windows.Forms.Button();
+            this.gomb3 = new System.Windows.Forms.Button();
+            this.gomb4 = new System.Windows.Forms.Button();
+            this.gomb5 = new System.Windows.Forms.Button();
+            this.gomb6 = new System.Windows.Forms.Button();
+            this.gomb7 = new System.Windows.Forms.Button();
+            this.gomb8 = new System.Windows.Forms.Button();
+            this.gomb9 = new System.Windows.Forms.Button();
+            this.plusz = new System.Windows.Forms.Button();
+            this.minusz = new System.Windows.Forms.Button();
+            this.egyenlo = new System.Windows.Forms.Button();
+            this.szoroz = new System.Windows.Forms.Button();
+            this.oszt = new System.Windows.Forms.Button();
+            this.vesszo = new System.Windows.Forms.Button();
+            this.kikapcs = new System.Windows.Forms.Button();
+            this.urit = new System.Windows.Forms.Button();
+            this.pozneg = new System.Windows.Forms.Button();
+            this.kimenet = new System.Windows.Forms.TextBox();
+            this.gomb0 = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gomb1
+            // 
+            this.gomb1.Location = new System.Drawing.Point(22, 285);
+            this.gomb1.Name = "gomb1";
+            this.gomb1.Size = new System.Drawing.Size(75, 23);
+            this.gomb1.TabIndex = 0;
+            this.gomb1.Text = "1";
+            this.gomb1.UseVisualStyleBackColor = true;
+            this.gomb1.Click += new System.EventHandler(this.gomb1_Click);
+            // 
+            // gomb2
+            // 
+            this.gomb2.Location = new System.Drawing.Point(134, 285);
+            this.gomb2.Name = "gomb2";
+            this.gomb2.Size = new System.Drawing.Size(75, 23);
+            this.gomb2.TabIndex = 1;
+            this.gomb2.Text = "2";
+            this.gomb2.UseVisualStyleBackColor = true;
+            this.gomb2.Click += new System.EventHandler(this.gomb2_Click);
+            // 
+            // gomb3
+            // 
+            this.gomb3.Location = new System.Drawing.Point(245, 285);
+            this.gomb3.Name = "gomb3";
+            this.gomb3.Size = new System.Drawing.Size(75, 23);
+            this.gomb3.TabIndex = 2;
+            this.gomb3.Text = "3";
+            this.gomb3.UseVisualStyleBackColor = true;
+            this.gomb3.Click += new System.EventHandler(this.gomb3_Click);
+            // 
+            // gomb4
+            // 
+            this.gomb4.Location = new System.Drawing.Point(22, 324);
+            this.gomb4.Name = "gomb4";
+            this.gomb4.Size = new System.Drawing.Size(75, 23);
+            this.gomb4.TabIndex = 5;
+            this.gomb4.Text = "4";
+            this.gomb4.UseVisualStyleBackColor = true;
+            this.gomb4.Click += new System.EventHandler(this.gomb4_Click);
+            // 
+            // gomb5
+            // 
+            this.gomb5.Location = new System.Drawing.Point(134, 324);
+            this.gomb5.Name = "gomb5";
+            this.gomb5.Size = new System.Drawing.Size(75, 23);
+            this.gomb5.TabIndex = 4;
+            this.gomb5.Text = "5";
+            this.gomb5.UseVisualStyleBackColor = true;
+            this.gomb5.Click += new System.EventHandler(this.gomb5_Click);
+            // 
+            // gomb6
+            // 
+            this.gomb6.Location = new System.Drawing.Point(245, 324);
+            this.gomb6.Name = "gomb6";
+            this.gomb6.Size = new System.Drawing.Size(75, 23);
+            this.gomb6.TabIndex = 3;
+            this.gomb6.Text = "6";
+            this.gomb6.UseVisualStyleBackColor = true;
+            this.gomb6.Click += new System.EventHandler(this.gomb6_Click);
+            // 
+            // gomb7
+            // 
+            this.gomb7.Location = new System.Drawing.Point(22, 363);
+            this.gomb7.Name = "gomb7";
+            this.gomb7.Size = new System.Drawing.Size(75, 23);
+            this.gomb7.TabIndex = 8;
+            this.gomb7.Text = "7";
+            this.gomb7.UseVisualStyleBackColor = true;
+            this.gomb7.Click += new System.EventHandler(this.gomb7_Click);
+            // 
+            // gomb8
+            // 
+            this.gomb8.Location = new System.Drawing.Point(134, 363);
+            this.gomb8.Name = "gomb8";
+            this.gomb8.Size = new System.Drawing.Size(75, 23);
+            this.gomb8.TabIndex = 7;
+            this.gomb8.Text = "8";
+            this.gomb8.UseVisualStyleBackColor = true;
+            this.gomb8.Click += new System.EventHandler(this.gomb8_Click);
+            // 
+            // gomb9
+            // 
+            this.gomb9.Location = new System.Drawing.Point(245, 363);
+            this.gomb9.Name = "gomb9";
+            this.gomb9.Size = new System.Drawing.Size(75, 23);
+            this.gomb9.TabIndex = 6;
+            this.gomb9.Text = "9";
+            this.gomb9.UseVisualStyleBackColor = true;
+            this.gomb9.Click += new System.EventHandler(this.gomb9_Click);
+            // 
+            // plusz
+            // 
+            this.plusz.Location = new System.Drawing.Point(352, 324);
+            this.plusz.Name = "plusz";
+            this.plusz.Size = new System.Drawing.Size(75, 23);
+            this.plusz.TabIndex = 9;
+            this.plusz.Text = "+";
+            this.plusz.UseVisualStyleBackColor = true;
+            // 
+            // minusz
+            // 
+            this.minusz.Location = new System.Drawing.Point(352, 363);
+            this.minusz.Name = "minusz";
+            this.minusz.Size = new System.Drawing.Size(75, 23);
+            this.minusz.TabIndex = 10;
+            this.minusz.Text = "-";
+            this.minusz.UseVisualStyleBackColor = true;
+            // 
+            // egyenlo
+            // 
+            this.egyenlo.Location = new System.Drawing.Point(352, 404);
+            this.egyenlo.Name = "egyenlo";
+            this.egyenlo.Size = new System.Drawing.Size(75, 23);
+            this.egyenlo.TabIndex = 11;
+            this.egyenlo.Text = "=";
+            this.egyenlo.UseVisualStyleBackColor = true;
+            // 
+            // szoroz
+            // 
+            this.szoroz.Location = new System.Drawing.Point(352, 241);
+            this.szoroz.Name = "szoroz";
+            this.szoroz.Size = new System.Drawing.Size(75, 23);
+            this.szoroz.TabIndex = 12;
+            this.szoroz.Text = "*";
+            this.szoroz.UseVisualStyleBackColor = true;
+            // 
+            // oszt
+            // 
+            this.oszt.Location = new System.Drawing.Point(352, 285);
+            this.oszt.Name = "oszt";
+            this.oszt.Size = new System.Drawing.Size(75, 23);
+            this.oszt.TabIndex = 13;
+            this.oszt.Text = "/";
+            this.oszt.UseVisualStyleBackColor = true;
+            // 
+            // vesszo
+            // 
+            this.vesszo.Location = new System.Drawing.Point(22, 404);
+            this.vesszo.Name = "vesszo";
+            this.vesszo.Size = new System.Drawing.Size(75, 23);
+            this.vesszo.TabIndex = 14;
+            this.vesszo.Text = ",";
+            this.vesszo.UseVisualStyleBackColor = true;
+            this.vesszo.Click += new System.EventHandler(this.vesszo_Click);
+            // 
+            // kikapcs
+            // 
+            this.kikapcs.Location = new System.Drawing.Point(22, 241);
+            this.kikapcs.Name = "kikapcs";
+            this.kikapcs.Size = new System.Drawing.Size(75, 23);
+            this.kikapcs.TabIndex = 15;
+            this.kikapcs.Text = "Off";
+            this.kikapcs.UseVisualStyleBackColor = true;
+            this.kikapcs.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // urit
+            // 
+            this.urit.Location = new System.Drawing.Point(134, 241);
+            this.urit.Name = "urit";
+            this.urit.Size = new System.Drawing.Size(75, 23);
+            this.urit.TabIndex = 16;
+            this.urit.Text = "Clear";
+            this.urit.UseVisualStyleBackColor = true;
+            // 
+            // pozneg
+            // 
+            this.pozneg.Location = new System.Drawing.Point(245, 404);
+            this.pozneg.Name = "pozneg";
+            this.pozneg.Size = new System.Drawing.Size(75, 23);
+            this.pozneg.TabIndex = 17;
+            this.pozneg.Text = "+-";
+            this.pozneg.UseVisualStyleBackColor = true;
+            // 
+            // kimenet
+            // 
+            this.kimenet.Location = new System.Drawing.Point(22, 79);
+            this.kimenet.Multiline = true;
+            this.kimenet.Name = "kimenet";
+            this.kimenet.ReadOnly = true;
+            this.kimenet.Size = new System.Drawing.Size(405, 73);
+            this.kimenet.TabIndex = 18;
+            this.kimenet.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // gomb0
+            // 
+            this.gomb0.Location = new System.Drawing.Point(134, 404);
+            this.gomb0.Name = "gomb0";
+            this.gomb0.Size = new System.Drawing.Size(75, 23);
+            this.gomb0.TabIndex = 19;
+            this.gomb0.Text = "0";
+            this.gomb0.UseVisualStyleBackColor = true;
+            this.gomb0.Click += new System.EventHandler(this.gomb0_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(245, 241);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 20;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // Szamologep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(448, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.gomb0);
+            this.Controls.Add(this.kimenet);
+            this.Controls.Add(this.pozneg);
+            this.Controls.Add(this.urit);
+            this.Controls.Add(this.kikapcs);
+            this.Controls.Add(this.vesszo);
+            this.Controls.Add(this.oszt);
+            this.Controls.Add(this.szoroz);
+            this.Controls.Add(this.egyenlo);
+            this.Controls.Add(this.minusz);
+            this.Controls.Add(this.plusz);
+            this.Controls.Add(this.gomb7);
+            this.Controls.Add(this.gomb8);
+            this.Controls.Add(this.gomb9);
+            this.Controls.Add(this.gomb4);
+            this.Controls.Add(this.gomb5);
+            this.Controls.Add(this.gomb6);
+            this.Controls.Add(this.gomb3);
+            this.Controls.Add(this.gomb2);
+            this.Controls.Add(this.gomb1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Szamologep";
+            this.ShowIcon = false;
+            this.Text = "Számológép";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button gomb1;
+        private System.Windows.Forms.Button gomb2;
+        private System.Windows.Forms.Button gomb3;
+        private System.Windows.Forms.Button gomb4;
+        private System.Windows.Forms.Button gomb5;
+        private System.Windows.Forms.Button gomb6;
+        private System.Windows.Forms.Button gomb7;
+        private System.Windows.Forms.Button gomb8;
+        private System.Windows.Forms.Button gomb9;
+        private System.Windows.Forms.Button plusz;
+        private System.Windows.Forms.Button egyenlo;
+        private System.Windows.Forms.Button szoroz;
+        private System.Windows.Forms.Button oszt;
+        private System.Windows.Forms.Button vesszo;
+        private System.Windows.Forms.Button kikapcs;
+        private System.Windows.Forms.Button urit;
+        private System.Windows.Forms.Button pozneg;
+        private System.Windows.Forms.Button minusz;
+        private System.Windows.Forms.TextBox kimenet;
+        private System.Windows.Forms.Button gomb0;
+        private System.Windows.Forms.Button delete;
+    }
+}
+
---
+++ Számológép/Szamologep.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Számológép
+{
+    public partial class Szamologep : Form
+    {
+        int osszeg = 0;
+        string muvelet = "0";
+        int db = 0;
+        public Szamologep()
+        {
+            InitializeComponent();
+            kimenet.Text = Convert.ToString(osszeg);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void gomb1_Click(object sender, EventArgs e)
+        {
+            if (db==0)
+            {
+                muvelet = gomb1.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb2_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb2.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb3_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb3.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb4_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb4.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb5_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb5.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb6_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb6.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb7_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb7.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb8_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb8.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb9_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb9.Text;
+            }
+            db = db + 1;
+        }
+
+        private void gomb0_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = gomb0.Text;
+            }
+            db = db + 1;
+        }
+
+        private void vesszo_Click(object sender, EventArgs e)
+        {
+            if (db == 0)
+            {
+                muvelet = muvelet + vesszo.Text;
+            }
+            db = db + 1;
+        }
+    }
+}
